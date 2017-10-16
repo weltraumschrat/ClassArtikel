2b5d22d89deb708b6642ca8bff956d5993dc40da
weltraumschrat <intergalacticrailroadcompany@gmail.com>
2017-10-16 15:27:00 +0200

    123

---
+++ Artikel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtikelKlasse
+{
+    class Artikel
+    {
+        private int _id;
+        private string _bezeichnung;
+        private double _volumen;
+        private double _preis;
+
+        public Artikel()
+        {
+        }
+        public Artikel(int id)
+        {
+            _id = id;
+            _bezeichnung = Program.testArtikel[id - 1, 0];
+            _volumen = Convert.ToDouble(Program.testArtikel[id - 1, 1]);
+            _preis = Convert.ToDouble(Program.testArtikel[id - 1, 2]);
+        }
+        public void Show()
+        {
+            Console.WriteLine("Artikel #{0} ist {1}, hat ein Volumen von {2} und einen Preis von {3:C}", _id, _bezeichnung,_volumen,_preis);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ArtikelKlasse
+{
+    
+    class Program
+    {
+        public static string[,] testArtikel = { { "Kürbis", "2", "1,79" }, { "Forelle", "1,2", "4,99" }, { "Hundefutter", "6", "11,99" } };
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+
+
+            Artikel[] neuerArtikel = { new Artikel(1), new Artikel(2), new Artikel(3) };
+
+            foreach(Artikel produkt in neuerArtikel)
+            {
+                produkt.Show();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
